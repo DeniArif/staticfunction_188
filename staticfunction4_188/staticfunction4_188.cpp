e72179a17746d86c41294c47e4f997f703a756ce
DeniArif <deniarifwibowo82@gmail.com>
2023-05-15 18:50:04 +0700

    comit 4

---
+++ staticfunction4_188/staticfunction4_188.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class angka {
+private:
+	int *arr;
+	int panjang;
+public:
+	angka(int);//constructor
+	angka();//Destructor
+
+	void cetakData();
+	void isiData();
+};
+//Definisi member functon
+angka::angka(int i) {//Constructor
+	panjang = i;
+	arr = new int[i]
+}
